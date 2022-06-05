1472033af1bbe375e90fae2a9233d7dedb256d86
RobSmeeth <98979411+RobSmeeth@users.noreply.github.com>
2022-06-05 23:16:43 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+const slides = document.querySelectorAll('.slide')
+
 let page = document.querySelector('.page');
 let themeButton = document.querySelector('.theme-button');
 themeButton.onclick = function (){
 page.classList.toggle('dark-theme');
 };
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses()
+    
+        slide.classList.add('active')
+    })
+    }
+    
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
